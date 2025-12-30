01667dba47e58e836c072f95cc6c3dbb8c45f1de
neverspaik <neverspaik@gmail.com>
2025-12-30 11:28:02 +0300

    favicon update 30.12

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Bitrix Dev</title>
+
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+
     @vite(['resources/js/app.js', 'resources/sass/app.scss'])
 </head>
 <body class="flex flex-col min-h-screen">
